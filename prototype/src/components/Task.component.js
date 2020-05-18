48b2b0d9b6fc5901aabade2459fbdb9a833d7225
Adam <adam.swiatek@priint.com>
2020-05-18 09:52:10 +0200

    fd

--- prototype/src/components/Task.component.js
+++ prototype/src/components/Task.component.js
@@ -1,8 +1,7 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import TreeGrid from "./TreeGrid/TreeGrid";
 
 export default function Task() {
-
   function createData(name, calories, fat, carbs, protein, price) {
     const elementsList = [];
 
@@ -13,7 +12,7 @@ export default function Task() {
       carbs,
       protein,
       price,
-      children: elementsList
+      children: elementsList,
     };
   }
 
@@ -22,11 +21,11 @@ export default function Task() {
     createData("Ice cream sandwich", 237, 9.0, 37, 4.3, 4.99),
     createData("Eclair", 262, 16.0, 24, 6.0, 3.79),
     createData("Cupcake", 305, 3.7, 67, 4.3, 2.5),
-    createData("Gingerbread", 356, 16.0, 49, 3.9, 1.5)
+    createData("Gingerbread", 356, 16.0, 49, 3.9, 1.5),
   ]);
 
   const handleExpandChange = (e) => {
-    if(!e.row.children || e.row.children.length == 0) {
+    if (!e.row.children || e.row.children.length == 0) {
       let rowsState1 = [...rowsState];
       const children = [
         createData("aaaaaa" + Math.random(), 159, 6.0, 24, 4.0, 3.99),
@@ -35,56 +34,60 @@ export default function Task() {
 
       setState(rowsState1);
     }
-  }
+  };
 
-  return <TreeGrid rows={rowsState}
-                   columns={[
-                     {
-                       width: 200,
-                       label: "Dessert (100g serving)",
-                       dataKey: "name",
-                       visible: true,
-                     },
-                     {
-                       width: 100,
-                       label: "Calories",
-                       dataKey: "calories",
-                       numeric: false,
-                       visible: true,
-                       align: "right",
-                     },
-                     {
-                       width: 100,
-                       label: "Fat (g)",
-                       dataKey: "fat",
-                       numeric: false,
-                       visible: true,
-                       align: "right",
-                     },
-                     {
-                       width: 100,
-                       label: "Carbs (g)",
-                       dataKey: "carbs",
-                       numeric: false,
-                       visible: true,
-                       align: "right",
-                     },
-                     {
-                       width: 100,
-                       label: "Protein (g)",
-                       dataKey: "protein",
-                       numeric: false,
-                       visible: true,
-                       align: "right",
-                     },
-                     {
-                       width: 100,
-                       label: "Price",
-                       dataKey: "price",
-                       numeric: true,
-                       visible: true,
-                       align: "right",
-                     },
-                   ]}
-                   onExpand={handleExpandChange}/>;
+  return (
+    <TreeGrid
+      rows={rowsState}
+      columns={[
+        {
+          width: 200,
+          label: "Dessert (100g serving)",
+          dataKey: "name",
+          visible: true,
+        },
+        {
+          width: 100,
+          label: "Calories",
+          dataKey: "calories",
+          numeric: false,
+          visible: true,
+          align: "right",
+        },
+        {
+          width: 100,
+          label: "Fat (g)",
+          dataKey: "fat",
+          numeric: false,
+          visible: true,
+          align: "right",
+        },
+        {
+          width: 100,
+          label: "Carbs (g)",
+          dataKey: "carbs",
+          numeric: false,
+          visible: true,
+          align: "right",
+        },
+        {
+          width: 100,
+          label: "Protein (g)",
+          dataKey: "protein",
+          numeric: false,
+          visible: true,
+          align: "right",
+        },
+        {
+          width: 100,
+          label: "Price",
+          dataKey: "price",
+          numeric: true,
+          visible: true,
+          align: "right",
+        },
+      ]}
+      onExpand={handleExpandChange}
+    />
+  );
 }
--- prototype/src/components/TreeGrid/TreeGrid.js
+++ prototype/src/components/TreeGrid/TreeGrid.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -6,75 +6,74 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
-import TreeRow from "./TreeRow"
-import TreeCell from "./TreeCell"
+import TreeRow from "./TreeRow";
+import TreeCell from "./TreeCell";
 
 export default function CollapsibleTable(props) {
-    const [rowsState, setRows] = React.useState([]);
-    const [columnssState, setColumns] = React.useState([]);
-    const {rows, columns, onExpand} = props;
+  const [rowsState, setRows] = React.useState([]);
+  const [columnssState, setColumns] = React.useState([]);
+  const { rows, columns, onExpand } = props;
 
-    useEffect(() => {
-        generateRows();
-    },[rows]);
+  useEffect(() => {
+    generateRows();
+  }, [rows]);
 
-    useEffect(() => {
-        const elementsList = [];
-        createCellsElements(elementsList, columns);
-        setColumns(elementsList);
-    },[columns]);
+  useEffect(() => {
+    const elementsList = [];
+    createCellsElements(elementsList, columns);
+    setColumns(elementsList);
+  }, [columns]);
 
-    const expandedEvent = (e) =>{
-        if(!e.isExpanded){
-            e.row.expanded = false;
-
-        } else {
-            e.row.expanded = true;
-        }
-        generateRows();
+  const expandedEvent = (e) => {
+    if (!e.isExpanded) {
+      e.row.expanded = false;
+    } else {
+      e.row.expanded = true;
     }
+    generateRows();
+  };
 
-    const generateRows = () =>{
-        const elementsList = [];
-        createRowElements(elementsList, rows, 0);
-        setRows(elementsList);
+  const generateRows = () => {
+    const elementsList = [];
+    createRowElements(elementsList, rows, 0);
+    setRows(elementsList);
+  };
+
+  const createCellsElements = (cellElementsList, cellElements) => {
+    for (const element of cellElements) {
+      cellElementsList.push(<TreeCell key={element.name} column={element} />);
     }
+    return cellElementsList;
+  };
 
-    const createCellsElements = (cellElementsList, cellElements) => {
-
-        cellElementsList.push(<TableCell />);
-        for (const element of cellElements) {
-            cellElementsList.push(<TreeCell key={element.name} column={element}/>);
-        }
-        return cellElementsList;
+  const createRowElements = (rowElementsList, elementsRows, depth) => {
+    for (const element of elementsRows) {
+      rowElementsList.push(
+        <TreeRow
+          key={element.name}
+          depth={depth}
+          onExpand={(e) => {
+            onExpand(e);
+            expandedEvent(e);
+          }}
+          row={element}
+        />
+      );
+      if (element.children && (element.expanded === null || element.expanded)) {
+        createRowElements(rowElementsList, element.children, depth + 15);
+      }
     }
+    return rowElementsList;
+  };
 
-    const createRowElements = (rowElementsList, elementsRows, depth) => {
-        for (const element of elementsRows) {
-            rowElementsList.push(<TreeRow key={element.name} depth={depth} onExpand={ (e) => {onExpand(e);  expandedEvent(e)}} row={element}/>);
-            if(element.children && (element.expanded === null || element.expanded)){
-                createRowElements(rowElementsList, element.children, depth + 15);
-            }
-        }
-        return rowElementsList;
-    }
-
-    return (
-        <TableContainer component={Paper}>
-            <Table aria-label="collapsible table">
-                <TableHead>
-                    <TableRow>
-                        {
-                            columnssState
-                        }
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {
-                        rowsState
-                    }
-                </TableBody>
-            </Table>
-        </TableContainer>
-    );
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="collapsible table">
+        <TableHead>
+          <TableRow>{columnssState}</TableRow>
+        </TableHead>
+        <TableBody>{rowsState}</TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
