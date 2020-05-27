054daedbabd6426f78ad25b2f14f5f9b45609b1d
Adam <adam.swiatek@priint.com>
2020-05-27 13:39:12 +0200

    small changes

--- prototype/src/components/flatPlanning/FlatPlanning.component.js
+++ prototype/src/components/flatPlanning/FlatPlanning.component.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useRef, useEffect } from "react";
 import TreeGrid from "../customTreeGrid/CustomTreeGrid.component";
 import "./FlatPlanning.style.css";
 import Splitter from "m-react-splitters";
@@ -6,14 +6,28 @@ import "m-react-splitters/lib/splitters.css";
 import DragAndDrop from "../utils/DragAndDrop";
 import BoxTarget from "./BoxTarget";
 import TemplateGrid from "../template/TemplateGrid";
+import Button from "@material-ui/core/Button";
 
 export default function FlatPlanning() {
+  const splitterEl = useRef(null);
+
+  const toggle = () => {
+    const primaryPane = document.querySelector(".vertical.primary");
+    if (primaryPane) {
+      primaryPane.style.width !== "calc(100% - 10px)"
+        ? (primaryPane.style.width = "calc(100% - 10px)")
+        : (primaryPane.style.width = "70%");
+      window.dispatchEvent(new Event("resize"));
+    }
+  };
+
   return (
     <DragAndDrop>
       <div
         style={{
           display: "flex",
           width: "100%",
+          flexDirection: "column",
           height: "100%",
           backgroundColor: "transparent",
           overflowY: "auto",
@@ -23,12 +37,18 @@ export default function FlatPlanning() {
         <Splitter
           position="vertical"
           primaryPaneMinWidth={0}
-          primaryPaneMaxWidth={"100%"}
+          primaryPaneMaxWidth={"calc(100% - 10px)"}
           primaryPaneWidth="70%"
           dispatchResize={true}
           postPoned={false}
+          ref={splitterEl}
         >
-          <BoxTarget />
+          <>
+            <Button style={{}} variant="contained" onClick={toggle}>
+              Assign content
+            </Button>
+            <BoxTarget />
+          </>
           <div style={{ width: "100%", height: "100%", padding: "3px 3px" }}>
             <Splitter
               position="horizontal"
@@ -56,12 +76,7 @@ export default function FlatPlanning() {
                 document
                   .querySelector(".handle-bar.vertical")
                   .addEventListener("dblclick", () => {
-                    const primaryPane = document.querySelector(
-                      ".vertical.primary"
-                    );
-                    primaryPane.style.width !== "calc(100% - 10px)"
-                      ? (primaryPane.style.width = "calc(100% - 10px)")
-                      : (primaryPane.style.width = "70%");
+                    toggle();
                   })
               )}
             </Splitter>
